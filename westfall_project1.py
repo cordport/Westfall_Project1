9cf146338e9ef19ef5eb9473730d41a4e9d0d7c6
cmadams7 <cmadams7@liberty.edu>
2015-03-17 15:36:41 -0400

    Adding delete directory module and utility function to check if directory exists

--- westfall_project1.py
+++ westfall_project1.py
@@ -55,6 +55,43 @@ def download_BIN(ftpObj, fileName):
 
 
 #*********************
+#Deletes a directory from the server
+#ftpObj is the current working ftp server object
+#directoryName is the directory you want deleted
+#RETURN is False if error occurs else true
+#*********************
+def delete_Dir(ftpObj, dirName):
+    try:
+        if dir_Exists(ftpObj, dirName): #if the directory exists
+            ftpObj.rmd(dirName) #remove the directory
+            print("Directory: ", dirName, " deletion successful ") #print successful deletion message
+        else: #otherwise, the directory does not exist
+            print("Directory: ", dirName, " does not exist") #print directory DNE message
+            return False
+    except ftplib.all_errors: #an error occurred
+        print("Directory: ", dirName, " deletion failed") #print failed deletion message
+        return False #return false indicates deletion did not occur
+    return True
+
+
+#*********************
+#Checks if directory exists (in current location)
+#ftpObj is the current working ftp server object
+#dir is the directory you want to check for existence
+#THIS ASSUMES YOU ARE IN THE CORRECT DIR
+#RETURN is True if the directory exists, False if it does not
+#*********************
+def dir_Exists(ftpObj, dirName):
+    fileList = [] #list object to store all files
+    ftpObj.retrlines('LIST', fileList.append) #append to list
+    
+    for file in fileList: #for every file/directory in the list (current location)
+        if file.split()[-1] == dirName and file.upper().startswith('D'): # If the name matches and is a directory
+            return True #directory found, return True
+    return False #otherwise, directory not found, return False
+
+
+#*********************
 #Disconnects from the ftp server, steralizes ftpObj
 #Should be called whenever switching servers or closing the program
 #ftpObj is a active ftp connection object
